5dd7007dfa68139554e7b24cabdfea6e5906882d
Adam Piontek <adam@73k.us>
2023-06-21 09:08:07 -0400

    added linting & formatting via eslint & prettier

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
 module.exports = {
-	title: "73k",
-	url: "https://73k.us/",
-	language: "en",
-	description: "Adam Piontek, on the internet. Personal website, blog, home page.",
-	author: {
-		name: "Adam Piontek",
-		email: "adam@73k.us",
-		url: "https://73k.us/about/"
-	}
+  title: "73k",
+  url: "https://73k.us/",
+  language: "en",
+  description:
+    "Adam Piontek, on the internet. Personal website, blog, home page.",
+  author: {
+    name: "Adam Piontek",
+    email: "adam@73k.us",
+    url: "https://73k.us/about/",
+  },
 }
